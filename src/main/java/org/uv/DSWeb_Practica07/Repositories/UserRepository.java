6ba1020135928570c545fe0de7d1a183507b73e1
Aldo Torres Ramírez <zS20006781@estudiantes.uv.mx>
2023-12-14 13:05:58 -0600

    Aldo commmit

---
+++ src/main/java/org/uv/DSWeb_Practica07/Repositories/UserRepository.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/springframework/Repository.java to edit this template
+ */
+
+ 
+package org.uv.DSWeb_Practica07.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.uv.DSWeb_Practica07.Models.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
